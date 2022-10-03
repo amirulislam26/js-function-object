37d7ce9267ae5b983f11d87ba440d0db02fe4c27
amirulislam26 <26techamirulislam@gmail.com>
2022-10-02 22:56:36 -0700

    added my function solved

---
+++ solved.js
@@ -0,0 +1,29 @@
+//...............01.................
+function bringSingara(money){
+    console.log('mama taka disen :', money);
+    console.log('ai nen singara');
+    
+}
+var taka = 300;
+// bringSingara(taka);
+//...............02....................
+function love(ami, tumi){
+    console.log('i love you my jan', ami, tumi);
+}
+
+// love(100);
+
+// ...........03...............
+function love(ami, tumi){
+    console.log('i love you my jan', ami, tumi);
+}
+
+// love(100, 95);
+//------------04-----------------------
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(12, 15, 48, 50, 65);
