5f0c80137bc0b7a87b9cad7f551994098d53f9e0
amirulislam26 <26techamirulislam@gmail.com>
2022-10-02 22:25:13 -0700

    js function object declaration

---
+++ function.js
@@ -0,0 +1,14 @@
+// function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+// call the function......................
+startFan();
+console.log('i am a professional web degainer')
+startFan();
+console.log('i am a professional freelencer')
+startFan();
+console.log('i am a professional web &  degainer')
