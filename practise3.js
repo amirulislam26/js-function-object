a81987988f85590c6cb98f068603e7e3e64ce33f
amirulislam26 <26techamirulislam@gmail.com>
2022-10-03 09:41:31 -0700

    my practise3 added a switch case

---
+++ practise3.js
@@ -0,0 +1,12 @@
+const color = 'green';
+switch(color){
+    case 'red':
+        console.log('red single a no road par');
+        break;
+    case 'yellow':
+        console.log('yellow is the trafick jebra crocing papapar');
+        break;
+    case 'green':
+        console.log('green start the car');
+        break;
+}
