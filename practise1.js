bdbf255fcb6a398cc1e10874224b20b7c4af24ac
amirulislam26 <26techamirulislam@gmail.com>
2022-10-03 09:15:38 -0700

    my practise01 addeda commit

---
+++ practise1.js
@@ -0,0 +1,10 @@
+function foo(){
+    console.log('i am a web developer');
+    console.log('how are you');
+    console.log('i am fine and you!');
+}
+var bar = 'i am a professional web &  degainer';
+foo()
+console.log(bar)
+
+
