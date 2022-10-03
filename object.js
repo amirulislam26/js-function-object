40290b42ebcc308451832101708172a4f9a8d072
amirulislam26 <26techamirulislam@gmail.com>
2022-10-03 07:36:05 -0700

    added my js object declaration

---
+++ object.js
@@ -0,0 +1,22 @@
+var student = {
+    id: 115,
+    name: 'Amir Khan',
+    class: 9, 
+    marks: 98};
+
+    var mobile = {
+        brand: 'vivo',
+        price: 19000,
+        rem: '8gb',
+        cammera: '12mp'
+    };
+
+    var myComputer = {
+        brand: 'hP',
+        price: 50000,
+        color: 'black',
+        processor: 'i7',
+    };
+    console.log(myComputer);
+    myComputer.processor = 'i11';
+    console.log(myComputer);
